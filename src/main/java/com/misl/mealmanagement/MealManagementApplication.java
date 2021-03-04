ded3dec4f34d236e3b6f6661b07de2cd25f9a035
azmain <azmain@mislbd.com>
2021-03-04 11:09:19 +0600

    initial commit

---
+++ src/main/java/com/misl/mealmanagement/MealManagementApplication.java
@@ -0,0 +1,13 @@
+package com.misl.mealmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealManagementApplication.class, args);
+	}
+
+}
